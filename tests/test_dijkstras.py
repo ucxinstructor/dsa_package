8752a16232b933e04cf0eef991ef5eb73636d170
limsico <limsico@yahoo.com>
2024-12-23 10:24:23 -0800

    added test dijkstras

---
+++ tests/test_dijkstras.py
@@ -0,0 +1,34 @@
+import unittest
+from dsa.dijkstras import shortest_path
+from dsa.graph import AdjacencyListWeightedGraph
+
+class TestDijkstra(unittest.TestCase):
+    def setUp(self):
+        self.graph = AdjacencyListWeightedGraph()
+        self.graph.add_edge('A', 'B', 1)
+        self.graph.add_edge('A', 'C', 4)
+        self.graph.add_edge('B', 'C', 2)
+        self.graph.add_edge('B', 'D', 5)
+        self.graph.add_edge('C', 'D', 1)
+
+    def test_shortest_path(self):
+        weight_table, previous = shortest_path(self.graph, 'A', 'D')
+        self.assertEqual(weight_table['D'], 4)
+        self.assertEqual(previous['D'], 'C')
+        self.assertEqual(previous['C'], 'B')
+        self.assertEqual(previous['B'], 'A')
+
+    def test_shortest_path_same_start_end(self):
+        weight_table, previous = shortest_path(self.graph, 'A', 'A')
+        self.assertEqual(weight_table['A'], 0)
+        self.assertEqual(previous['A'], 'A')
+
+    def test_shortest_path_no_path(self):
+#        self.graph.add_vertex('E')
+#        weight_table, previous = shortest_path(self.graph, 'A', 'E')
+#        self.assertNotIn('E', weight_table)
+#        self.assertNotIn('E', previous)
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
