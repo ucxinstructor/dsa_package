667f42905939102f91463b25fcf714137864902f
limsico <limsico@yahoo.com>
2025-01-17 20:43:04 -0800

    updated tests:

--- tests/test_hash.py
+++ tests/test_hash.py
@@ -4,18 +4,20 @@ from dsa.hashtable import HashTable
 
 class TestHashTable(unittest.TestCase):
     def test_create(self):
+        ''' test constructor '''
         ht = HashTable()
         self.assertEqual(ht.count, 0)
         self.assertEqual(ht.capacity, 20)
         ht = HashTable(1000)
         self.assertEqual(ht.count, 0)
-        self.assertEqual(ht.capacity, 100)
+        self.assertEqual(ht.capacity, 1000)
         ht = HashTable(5)
         self.assertEqual(ht.count, 0)
         self.assertEqual(ht.capacity, 5)
 
-    def test_insert(self):
-        ht = HashTable(1)
+    def test_set(self):
+        ''' test set method '''
+        ht = HashTable()
 
         self.assertEqual(ht.count, 0)
         ht.set("A", 1)
@@ -27,32 +29,20 @@ class TestHashTable(unittest.TestCase):
         self.assertEqual(ht.get("C"), 3)
         self.assertEqual(ht.count, 3)
 
+    def test_delete(self):
+        ''' test delete method'''
+        ht = HashTable()
         ht.delete("A")
-        ht.delete("D")
-        self.assertEqual(ht.count, 2)
-
-        ht.delete("B")
-        ht.delete("C")
-        ht.delete("X")
-        self.assertEqual(ht.count, 0)
-        ht.set("C", 30)
-        ht.set("C", 43)
-        self.assertEqual(ht.count, 1)
-        self.assertEqual(ht.get("A"), None)
-        self.assertEqual(ht.get("C"), 43)
-
-    def test_insert(self):
-        ht = HashTable(1)
 
         self.assertEqual(ht.count, 0)
         ht.set("A", 1)
         ht.set("B", 2)
         ht.set("C", 3)
         ht.set("A", 4)
-        self.assertEqual(ht.get("A"), 4)
-        self.assertEqual(ht.get("B"), 2)
-        self.assertEqual(ht.get("C"), 3)
-        self.assertEqual(ht.count, 3)
+
+        ht.delete("A")
+        ht.delete("D")
+        self.assertEqual(ht.count, 2)
 
         ht.delete("A")
         ht.delete("D")
@@ -62,8 +52,3 @@ class TestHashTable(unittest.TestCase):
         ht.delete("C")
         ht.delete("X")
         self.assertEqual(ht.count, 0)
-        ht.set("C", 30)
-        ht.set("C", 43)
-        self.assertEqual(ht.count, 1)
-        self.assertEqual(ht.get("A"), None)
-        self.assertEqual(ht.get("C"), 43)
