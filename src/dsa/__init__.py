5f0e4bd78779c15a510f8b38dfaeb88013f0dc1d
Carl Limsico <limsico@Carls-Laptop.local>
2025-02-09 17:00:30 -0800

    updated to version 2025.2.9.1

--- src/dsa/__init__.py
+++ src/dsa/__init__.py
@@ -3,5 +3,5 @@ dsa package for UC Berkeley Extension's Computer Science X404.1 Data Structures 
 
 Last Revision: 2/9/2025
 '''
-version = '2025.2.9'
-__version__ = '2025.2.9'
+version = '2025.2.9.1'
+__version__ = '2025.2.9.1'
