72a69c9edfb5c225111b51cdbf196daa7b1c33de
Carl Limsico <limsico@Carls-Laptop.local>
2025-12-23 19:57:09 -0800

    updated for version 2025.12.24

--- src/dsa/__init__.py
+++ src/dsa/__init__.py
@@ -1,7 +1,7 @@
 '''
 dsa package for UC Berkeley Extension's Computer Science X404.1 Data Structures and Algorithms class
 
-Last Revision: 12/9/2025
+Last Revision: 12/24/2025
 '''
-version = '2025.12.9'
-__version__ = '2025.12.9'
+version = '2025.12.24'
+__version__ = '2025.12.24'
